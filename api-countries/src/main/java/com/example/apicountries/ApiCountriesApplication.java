0e7728ac9685edf503ef4c46b449278dbcef0cb3
vaskocuturilo <vaskocuturilo@rammbler.ru>
2025-12-12 16:18:09 +0100

    DV-0008: Add Cors filter.

--- api-countries/src/main/java/com/example/apicountries/ApiCountriesApplication.java
+++ api-countries/src/main/java/com/example/apicountries/ApiCountriesApplication.java
@@ -2,8 +2,12 @@ package com.example.apicountries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableMongoRepositories
 public class ApiCountriesApplication {
 
     public static void main(String[] args) {
